a1484176440a6c87e0362b07d5ec0e9c514e4e9b
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-07-04 18:54:43 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 
-if (index.name === "Susan") {
+if (index.name === "Joe") {
     console.log("The name is correct")
 } 
 
-if (index.height === 70) {
+if (index.height === 74) {
     console.log("The height is correct")
 }
